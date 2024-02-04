7d9961faaff7462a34f23d8ba1de1ca351fd294d
Tomasz <n1kula@wp.pl>
2024-02-04 10:59:55 +0100

    Add project files.

---
+++ HelloWorld/HelloWorld/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using HelloWorld.ViewModels;
+using Xamarin.Forms;
+
+namespace HelloWorld.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
